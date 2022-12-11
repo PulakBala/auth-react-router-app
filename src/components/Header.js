861491c0384f06bcddc45ae53395af64b99216b0
PulakBala <pulakbala0524@gmail.com>
2022-12-11 19:12:11 +0600

    register or login button add

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Authenctication</Link>
 
-                <Link to='/'>Home</Link>
-                <Link to='/login'>Log in</Link>
-                <Link to='/register'>Register</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log in</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
             </div>
         </div>
     );
